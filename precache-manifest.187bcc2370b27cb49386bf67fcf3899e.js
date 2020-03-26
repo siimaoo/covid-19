7d74a5914305b77fda753854222b882ba164b848
Lucas simão <lucassimao@dialhost.com.br>
2020-03-26 19:55:30 -0300

    Updates

---
+++ precache-manifest.187bcc2370b27cb49386bf67fcf3899e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f942b7ac18799ba4d50d9d233577d25d",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "d61fd7dae29d3ac0d27f",
+    "url": "/covid-19/static/css/main.7b342aae.chunk.css"
+  },
+  {
+    "revision": "0361f00839be23c7a4d4",
+    "url": "/covid-19/static/js/2.b4bd39d2.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/covid-19/static/js/2.b4bd39d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d61fd7dae29d3ac0d27f",
+    "url": "/covid-19/static/js/main.44169f6c.chunk.js"
+  },
+  {
+    "revision": "8a52c8d37d83ffe3717e",
+    "url": "/covid-19/static/js/runtime-main.1a3018c3.js"
+  }
+]);
